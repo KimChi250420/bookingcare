dfb0aa6deba2285ce30819667c1b30d6f7a0bc2c
kinmchitran <kimchitrana2@gmail.com>
2023-05-25 18:12:36 +0700

    table

--- Frontend-React.JS-QuickStart/src/services/userService.js
+++ Frontend-React.JS-QuickStart/src/services/userService.js
@@ -3,4 +3,11 @@ const handleLoginApi = (email, password) => {
   return axios.post("/api/login", { email, password });
 };
 
-export { handleLoginApi };
+const getAllUsers = (inputId) => {
+  return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+const createNewUserService = (data) => {
+  console.log("check data from services: ", data);
+  return axios.post(`/api/create-new-user`, data);
+};
+export { handleLoginApi, getAllUsers, createNewUserService };
